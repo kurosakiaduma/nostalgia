e1f7379a7499bf83d43b7bfa1788f571af1cfb17
Tevin Aduma <tevin74@live.com>
2023-06-18 01:22:00 +0300

    define project settings, asgi, wsgi and requirements

--- nostalgia/asgi.py
+++ nostalgia/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nostalgia.settings")
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack  
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.sessions import SessionMiddlewareStack  
+from django.core.asgi import get_asgi_application
+from reactpy_django import REACTPY_WEBSOCKET_PATH 
+
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": SessionMiddlewareStack(
+            AuthMiddlewareStack(URLRouter([REACTPY_WEBSOCKET_PATH]))
+        ),
+    }
+)
--- nostalgia/settings.py
+++ nostalgia/settings.py
@@ -25,18 +25,22 @@ SECRET_KEY = "django-insecure-6$utp3*rj!j$fkmroe*y4m+=k24nkjm!@-h#pnx+0mnvk9fhz@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "uvicorn",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "graphene_django",
+    "reactpy_django",
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "nostalgia.urls"
@@ -54,7 +59,7 @@ ROOT_URLCONF = "nostalgia.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["frontend/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -67,7 +72,14 @@ TEMPLATES = [
     },
 ]
 
+ASGI_APPLICATION = "nostalgia.asgi.application"
 WSGI_APPLICATION = "nostalgia.wsgi.application"
+
+
+# Graphene schema location
+GRAPHENE = {
+    "SCHEMA": "django_root.schema.schema"
+}
 
 
 # Database
@@ -115,7 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = Path.joinpath(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, "frontend", "static")]
+
+# Whitenoise Configuration
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
