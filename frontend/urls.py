e63346f86c2635ed0b36752be02881a8d671cc64
Tevin Aduma <tevin74@live.com>
2023-06-20 16:46:25 +0300

    frontend urls config

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,10 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    #APIs
+    path('api/check-email', check_email, name='check-email'),
+    # Page routes
     path("", index, name="index"),
+    path("register", register_user, name="register_user"),
+    path("login", login_user, name="login_user"),
 ]
